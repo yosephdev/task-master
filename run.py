824e80948820581e1f685f5111fbf2bbc8f3fe0c
yosephdev <>
2024-02-06 15:01:18 +0000

    feat: Implement function to load tasks from JSON file

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
+import json
+
 tasks = []
+
+def load_tasks():
+    global tasks
+    try:
+        with open('tasks.json', 'r') as file:
+            tasks = json.load(file)
+    except FileFoundError:
+        tasks = []
