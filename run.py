c1d69afe08dda7d65a2746acf9baa5efec4607f0
yosephdev <>
2024-02-07 07:32:13 +0000

    fix: Correct unterminated string literal on load_tasks

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ tasks = []
 def load_tasks():
     global tasks
     try:
-        with open('tasks.json', r') as file:
+        with open('tasks.json', 'r') as file:
             tasks = json.load(file)
     except FileFoundError:
         tasks = []
